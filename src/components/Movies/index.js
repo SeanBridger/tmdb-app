4b4a6b73ba4f6404303f69a677f245a6497ac7a1
SeanBridger <seanbridger@gmail.com>
2016-11-01 19:55:05 +0000

    create config file

--- src/components/Movies/index.js
+++ src/components/Movies/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import MovieItem from '../MovieItem';
+import { IMAGE_URL } from '../../config';
 
 const Movies = ({max, movies, title}) => {
   let items = '';
   if(movies) {
     items = movies.slice(0, max).map(function(item, i) {
-      console.log('here');
       let poster = item.poster_path;
         if(!poster) {
           poster = 'http://placehold.it/600x900';
         } else {
-          poster = `http://image.tmdb.org/t/p/original${poster}`;
+          poster = `${IMAGE_URL}${poster}`;
         }
         return (
           <MovieItem key={item.id} id={item.id} poster={poster} />
