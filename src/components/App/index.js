9949354a124294539261750270af8cb465d016c5
SeanBridger <seanbridger@gmail.com>
2016-11-01 19:15:34 +0000

    move search field and movies list into Movies component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Header from '../Header';
 import Movies from '../Movies';
-import MovieItem from '../MovieItem';
 
 import './styles.css';
 
@@ -10,22 +9,7 @@ const App = () => {
   return (
     <div>
       <Header />
-      <div className="movies" >
-        <input placeholder="Search movies" />
-        <h1>Results</h1>
-        <Movies>
-          <MovieItem id="1" />
-          <MovieItem id="2" />
-          <MovieItem id="3" />
-          <MovieItem id="4" />
-          <MovieItem id="5" />
-          <MovieItem id="6" />
-          <MovieItem id="7" />
-          <MovieItem id="8" />
-          <MovieItem id="9" />
-          <MovieItem id="10" />
-        </Movies>
-      </div>
+      <Movies />
     </div>
   );
 };
--- src/components/Movies/index.js
+++ src/components/Movies/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
-const Movies = ({children}) => {
+import MovieItem from '../MovieItem';
+
+const Movies = () => {
   return (
+    <div className="movies">
+      <input placeholder="Search movies" />
+      <h1>Results</h1>
       <div className="movies-wrapper">
-        {children}
+        <MovieItem id="1" />
+        <MovieItem id="2" />
+        <MovieItem id="3" />
+        <MovieItem id="4" />
+        <MovieItem id="5" />
+        <MovieItem id="6" />
+        <MovieItem id="7" />
+        <MovieItem id="8" />
+        <MovieItem id="9" />
+        <MovieItem id="10" />
       </div>
+    </div>
   );
 };
 
